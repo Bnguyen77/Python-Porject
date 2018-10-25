b7d7cbec011410080f4465fb625ec447ce9a66a2
faizanrahman <faizanrahman89@gmail.com>
2018-10-24 19:54:10 -0500

    Updated models

--- apps/Price/models.py
+++ apps/Price/models.py
@@ -1,14 +1,41 @@
 from django.db import models
+from datetime import datetime
+from django.db.models import Max
+from django.db.models import Min
+
+# class ProductManager(models.Manager):
+#     def lowestPrice(self, productID):
+#         product = Stock.objects.filter(product_id=1)
+#         for i in product:
+              #print(i.price)
+
+#         #for price in Stock.objects.all() 
+#         #print(price.price)    
+#         #Count
+#         #Compare prices
+#         #Return lowest
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     details = models.TextField()
     category = models.CharField(max_length=45)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    #objects=ProductManager()
+
+
+
 
 class Retailer(models.Model):
     name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
 
 class Stock(models.Model):
     product_id = models.ForeignKey(Product, related_name='product_id')
     retailer_id = models.ForeignKey(Retailer, related_name='retailer_id')
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
--- apps/Price/views.py
+++ apps/Price/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+def index(request):
+    return render(request, "Price/logReg.html")
+def register(request):
+    return render(request, "Price/Register.html")
+def dashboard(request):
+    
+    return render(request, "Price/dashboard.html")
+def productCategory(request, category):
+    productInCategory = Product.objects.filter(category=category)
+    context = {
+        'productInCategory':productInCategory,
+    }
+
+    return render(request, "Price/products.html", context)
+def productPage(request, productId):
+    IndividualProduct = Stock.objects.filter(product_id=productId)
+    context = {
+        "IndividualProduct": IndividualProduct
+    }
+    return render(request, "Price/productpage.html")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#def compare(request, id):
+# Takes the id of the product clicked or searched.
+#Uses that id to get the price of that product from all retailers in the Retailer model
+#Compares all the prices and returns the lowest price.
+#Display the lowest price.
+#Display the highest price.
+#Display prices from all retailers as a list on the front-end. 
